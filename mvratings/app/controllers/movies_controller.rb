e6d770e278765a56936cb03408b2cb5736977ed5
swhitsett <kongzero32@gmail.com>
2016-06-20 01:31:06 -0700

    routes and notes

--- mvratings/app/controllers/movies_controller.rb
+++ mvratings/app/controllers/movies_controller.rb
@@ -5,11 +5,13 @@ class MoviesController < ApplicationController
   # GET /movies.json
   def index
     @movies = Movie.all
+
   end
 
   # GET /movies/1
   # GET /movies/1.json
   def show
+    @query = Tmdb::Search.movie(params[:user_input])
   end
 
   # GET /movies/new
@@ -64,7 +66,7 @@ class MoviesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
-      @movie = Movie.find(params[:id])
+      #@movie = Movie.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- mvratings/config/routes.rb
+++ mvratings/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :ratings
   resources :users
   root 'movies#index'
+  get 'query',to:'movies#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
